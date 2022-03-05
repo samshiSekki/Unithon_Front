f438c83f4e4668dc16a758fc184f4c296a9688d4
NamNamju <namuju_hi@naver.com>
2022-03-06 03:58:01 +0900

    conflict merge

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import Login from 'pages/Login';
 import Signup from 'pages/Signup';
 import LandingPage from 'pages/LandingPage/LandingPage.tsx';
 import MainPage from 'pages/MainPage';
+import CheckNote from 'pages/CheckNotePage/CheckNote.tsx';
+import CheckNoteTitle from 'pages/CheckNotePage/CheckNoteTitle';
+import MyNote from 'pages/MyNotePage/MyNote.tsx';
+import MyBeforeNote from 'pages/MyNotePage/MyBeforeNote';
 
 function App() {
   return (
@@ -13,7 +17,11 @@ function App() {
         <Route path='/login' element={<Login/>}/>
         <Route path='/' element={<LandingPage/>}/>
         <Route path='/signup' element={<Signup/>}/>
-        <Route path='/main' element={<MainPage/>}/>
+        <Route path='/main' element={<CheckNote/>}/>
+        {/* <Route path='/checknote' element={<CheckNote/>}/> */}
+        <Route path='/checknotetitle' element={<CheckNoteTitle/>}></Route>
+        <Route path='/mynote' element={<MyNote/>}></Route>
+        <Route path='/mybeforenote' element={<MyBeforeNote/>}></Route>
       </Routes>
     </div>
   );
